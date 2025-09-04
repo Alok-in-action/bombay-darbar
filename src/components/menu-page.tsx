9b2dc9f0d9501dcad046a4374c09df16e5a763c1
Alok Khamora <itsalokkhamora@gmail.com>
2025-09-04 09:53:00 +0000

    add bg.png as a logo from bglogo file , the png file is a logo and its b

--- src/components/menu-page.tsx
+++ src/components/menu-page.tsx
@@ -175,6 +175,7 @@ export function MenuPage({ menuData }: { menuData: MenuCategory[] }) {
         <div className="container mx-auto px-4">
           <div className="flex flex-col items-center justify-between gap-4 py-2 md:flex-row md:flex-wrap">
               <div className="flex flex-shrink-0 items-center text-center">
+                  <Image src="/bg.png" alt="Bombay Darbar Logo" width={60} height={60} className="h-15 w-15 mr-4"/>
                   <div className="flex flex-col">
                       <h1 className="font-headline text-3xl font-bold text-primary">Bombay Darbar</h1>
                       <span className="font-headline text-lg -mt-1 text-center">Hotel &amp; Restaurant</span>
@@ -227,7 +228,7 @@ export function MenuPage({ menuData }: { menuData: MenuCategory[] }) {
         </nav>
       )}
 
-      <main className="container mx-auto px-4 py-8" style={{ paddingTop: `${(activeView === 'menu' ? headerHeight + navHeight : headerHeight) + 32}px` }}>
+      <main className="container mx-auto px-4" style={{ paddingTop: `${(activeView === 'menu' ? headerHeight + navHeight : headerHeight) + 32}px` }}>
         {activeView === 'menu' ? (
           <>
             {filteredMenu.length > 0 ? (
@@ -298,3 +299,5 @@ export function MenuPage({ menuData }: { menuData: MenuCategory[] }) {
     </div>
   );
 }
+
+    
